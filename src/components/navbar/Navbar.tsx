12449c832d1a57223bd0c7b5654b4fa526edc657
Binary Digitz <borisndoh@gmail.com>
2024-12-19 10:31:24 +0100

    Adjust Navbar layout on mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { InputSearch, Logo, NavbarItems, NavbarMenu } from "../exportComp"
 
 function Navbar() {
   return (
-    <nav className="flex md:justify-around gap-3 justify-between items-center p-6 shadow-md text-[var(--secondary-color)]">
+    <nav className="flex md:justify-around gap-3 justify-between items-center p-3 py-4 lg:py-6 shadow-md text-[var(--secondary-color)]">
       <Logo/>
       <InputSearch onSearch={(search) => console.log(search)}/>
       <NavbarItems/>

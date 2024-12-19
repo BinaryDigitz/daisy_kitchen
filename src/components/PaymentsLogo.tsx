c9fae9bc61a338fbe1e5bfe664286a723d07990d
Binary Digitz <borisndoh@gmail.com>
2024-12-19 12:28:48 +0100

    Add payment logo on footer

---
+++ src/components/PaymentsLogo.tsx
@@ -0,0 +1,11 @@
+import {Visa, MasterCard} from './Icons/exportIcons'
+function PaymentsLogo() {
+  return (
+    <div>
+      <Visa/>
+      <MasterCard/>
+    </div>
+  )
+}
+
+export default PaymentsLogo

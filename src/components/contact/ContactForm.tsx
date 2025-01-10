34cb31780ebda3e246726f5da843107f3ca6f6f8
Binary Digitz <borisndoh@gmail.com>
2025-01-10 16:16:27 +0100

    Add contact details

---
+++ src/components/contact/ContactForm.tsx
@@ -0,0 +1,14 @@
+
+
+function ContactForm() {
+  return (
+    <form>
+       <div className="mb-3">
+            <label htmlFor="fullName">Full Name</label>
+            <input type="text" placeholder="John Doe" />
+          </div>
+    </form>
+  )
+}
+
+export default ContactForm

460c0f277772007ff2c9351dc491e7bb46f28fb3
Binary Digitz <borisndoh@gmail.com>
2025-03-31 17:39:20 +0100

    Complete Employee UI

--- admin/src/pages/Employees.tsx
+++ admin/src/pages/Employees.tsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { employees } from "../assets/assets";
 import EmployeeForm from "../components/EmployeeForm";
 import Title from "../components/Title";
 
 function Employees() {
+  const [showForm, setShowForm] = useState(true);
   return (
-    <div className="grid place-items-center mt-10 ">
+    <div className="grid place-items-center mt-10 relative">
       <Title text1="" text2="EMPLOYEES" size="heading3 text-red-900" />
-      <div className="hidden">
+      <button
+        onClick={() => setShowForm(!showForm)}
+        className="absolute top-5 left-5 border px-6 py-1 cursor-pointer hover:opacity-70 rounded-sm border-red-200 text-red-900"
+      >
+        {showForm ? "Hide form" : "Add Employee"}
+      </button>
+      <div className={`${!showForm && "hidden"}`}>
         <EmployeeForm />
       </div>
-      <div className="w-full mx-auto lg:w-3xl mt-10">
+      <div className="w-lg mx-auto lg:w-3xl mt-10">
         <table className="w-full">
           <thead>
             <tr className="flex font-medium  items-center justify-between lg:px-10 p-4 bg-green-50 border border-green-100 text-green-900 text-md  text-sm md:text-[18px]">
